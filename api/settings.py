a0f3a118858a0b2f6d7b9379ae7c50d251849912
Dennis Akagha <d.akagha20@gmail.com>
2023-09-10 14:48:28 +0100

    change the host and debug parameter

--- api/settings.py
+++ api/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-si8xklvg66-^zc&btmgfp_c)pp9i#jma*u$hrpvrs(m==yira6"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'denscholar.pythonanywhere.com']
 
 
 # Application definition
